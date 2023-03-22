74efa28f35d009bbe2f340f6d2740ca112468244
Pierre Burghardt <pierre.burghardt@student.hpi.uni-potsdam.de>
2023-03-22 10:56:56 +0100

    formatting

--- app/lib/objectivemodeler/OmModeler.js
+++ app/lib/objectivemodeler/OmModeler.js
@@ -1,5 +1,5 @@
 import inherits from 'inherits';
-import {findIndex } from 'min-dash'
+import {findIndex} from 'min-dash'
 
 import BaseModeler from './BaseModeler';
 
@@ -33,12 +33,12 @@ import PaletteModule from './features/palette';
 import ResizeModule from 'diagram-js/lib/features/resize';
 import SnappingModule from './features/snapping';
 import SpaceToolBehaviorModule from './behavior';
-import { nextPosition } from '../util/Util';
+import {nextPosition} from '../util/Util';
 import {is} from "bpmn-js/lib/util/ModelUtil";
 import modeling from './features/modeling';
 
 var initialDiagram =
-  `<?xml version="1.0" encoding="UTF-8"?>
+    `<?xml version="1.0" encoding="UTF-8"?>
 <od:definitions xmlns:od="http://tk/schema/od" xmlns:odDi="http://tk/schema/odDi">
     <od:odBoard id="Board" />
     <odDi:odRootBoard id="RootBoard" name="Objective 1">
@@ -47,7 +47,7 @@ var initialDiagram =
 </od:definitions>`;
 
 export default function OmModeler(options) {
-  BaseModeler.call(this, options);
+    BaseModeler.call(this, options);
 }
 
 inherits(OmModeler, BaseModeler);
@@ -57,20 +57,20 @@ OmModeler.Viewer = Viewer;
 OmModeler.NavigatedViewer = NavigatedViewer;
 
 /**
-* The createDiagram result.
-*
-* @typedef {Object} CreateDiagramResult
-*
-* @property {Array<string>} warnings
-*/
+ * The createDiagram result.
+ *
+ * @typedef {Object} CreateDiagramResult
+ *
+ * @property {Array<string>} warnings
+ */
 
 /**
-* The createDiagram error.
-*
-* @typedef {Error} CreateDiagramError
-*
-* @property {Array<string>} warnings
-*/
+ * The createDiagram error.
+ *
+ * @typedef {Error} CreateDiagramError
+ *
+ * @property {Array<string>} warnings
+ */
 
 /**
  * Create a new diagram to start modeling.
@@ -78,47 +78,47 @@ OmModeler.NavigatedViewer = NavigatedViewer;
  * @returns {Promise<CreateDiagramResult, CreateDiagramError>}
  *
  */
-OmModeler.prototype.createDiagram = function() {
-  const container = this.get('canvas').getContainer();
-  container.style.visibility = 'hidden';
-  return this.importXML(initialDiagram);
+OmModeler.prototype.createDiagram = function () {
+    const container = this.get('canvas').getContainer();
+    container.style.visibility = 'hidden';
+    return this.importXML(initialDiagram);
 };
 
 
 OmModeler.prototype._interactionModules = [
 
-  // non-modeling components
-  KeyboardMoveModule,
-  MoveCanvasModule,
-  OmButtonBarModule,
-  TouchModule,
-  ZoomScrollModule
+    // non-modeling components
+    KeyboardMoveModule,
+    MoveCanvasModule,
+    OmButtonBarModule,
+    TouchModule,
+    ZoomScrollModule
 ];
 
 OmModeler.prototype._modelingModules = [
 
-  // modeling components
-  AutoplaceModule,
-  AlignElementsModule,
-  AutoScrollModule,
-  BendpointsModule,
-  ConnectModule,
-  ConnectionPreviewModule,
-  ContextPadModule,
-  CopyPasteModule,
-  CreateModule,
-  EditorActionsModule,
-  GridSnappingModule,
-  KeyboardModule,
-  KeyboardMoveSelectionModule,
-  LabelEditingModule,
-  ModelingModule,
-  MoveModule,
-  OmObjectDropdown,
-  PaletteModule,
-  ResizeModule,
-  SnappingModule,
-  SpaceToolBehaviorModule
+    // modeling components
+    AutoplaceModule,
+    AlignElementsModule,
+    AutoScrollModule,
+    BendpointsModule,
+    ConnectModule,
+    ConnectionPreviewModule,
+    ContextPadModule,
+    CopyPasteModule,
+    CreateModule,
+    EditorActionsModule,
+    GridSnappingModule,
+    KeyboardModule,
+    KeyboardMoveSelectionModule,
+    LabelEditingModule,
+    ModelingModule,
+    MoveModule,
+    OmObjectDropdown,
+    PaletteModule,
+    ResizeModule,
+    SnappingModule,
+    SpaceToolBehaviorModule
 ];
 
 
@@ -129,137 +129,137 @@ OmModeler.prototype._modelingModules = [
 // - modeling modules
 
 OmModeler.prototype._modules = [].concat(
-  Viewer.prototype._modules,
-  OmModeler.prototype._interactionModules,
-  OmModeler.prototype._modelingModules
+    Viewer.prototype._modules,
+    OmModeler.prototype._interactionModules,
+    OmModeler.prototype._modelingModules
 );
 
 OmModeler.prototype.createObject = function (name) {
-  const modeling = this.get('modeling');
-  const canvas = this.get('canvas');
-  const diagramRoot = canvas.getRootElement();
+    const modeling = this.get('modeling');
+    const canvas = this.get('canvas');
+    const diagramRoot = canvas.getRootElement();
 
-  const {x,y} = nextPosition(this, 'om:Object');
-  const shape = modeling.createShape({
-    type: 'om:Object',
-    name: name
-  }, {x,y}, diagramRoot);
-  return shape.businessObject;
+    const {x, y} = nextPosition(this, 'om:Object');
+    const shape = modeling.createShape({
+        type: 'om:Object',
+        name: name
+    }, {x, y}, diagramRoot);
+    return shape.businessObject;
 }
 
 OmModeler.prototype.createName = function (name, clazz) {
-  const objectInstance = this.get('elementFactory').createObjectInstance(name, clazz);
-  this._definitions.get('objectInstances').push(objectInstance);
-  return objectInstance;
+    const objectInstance = this.get('elementFactory').createObjectInstance(name, clazz);
+    this._definitions.get('objectInstances').push(objectInstance);
+    return objectInstance;
 }
 
 OmModeler.prototype.renameObject = function (object, name) {
-  this.get('modeling').updateLabel(this.get('elementRegistry').get(object.id), name);
+    this.get('modeling').updateLabel(this.get('elementRegistry').get(object.id), name);
 }
 
 OmModeler.prototype.deleteObject = function (object) {
-  this.get('modeling').removeShape(object);
+    this.get('modeling').removeShape(object);
 }
 
 OmModeler.prototype.updateProperty = function (object, property) {
-  this.get('modeling').updateProperties(object, property);
+    this.get('modeling').updateProperties(object, property);
 }
 
-OmModeler.prototype.getObjectives = function() {
-  return this._definitions.get('rootBoards');
+OmModeler.prototype.getObjectives = function () {
+    return this._definitions.get('rootBoards');
 }
 
 OmModeler.prototype.showObjective = function (objective) {
-  const container = this.get('canvas').getContainer();
-  this._objective = objective;
-  this.clear();
-  if (objective) {
-    container.style.visibility = '';
-    this.open(objective);
-  } else {
-    container.style.visibility = 'hidden';
-  }
+    const container = this.get('canvas').getContainer();
+    this._objective = objective;
+    this.clear();
+    if (objective) {
+        container.style.visibility = '';
+        this.open(objective);
+    } else {
+        container.style.visibility = 'hidden';
+    }
 }
 
 OmModeler.prototype.getCurrentObjective = function () {
-  return this._objective;
+    return this._objective;
 }
 OmModeler.prototype.addObjective = function (name) {
-  var rootBoard = this.get('elementFactory').createRootBoard(name || '<TBD>');
-  this._definitions.get('rootBoards').push(rootBoard[0]);
-  this._definitions.get('rootElements').push(rootBoard[1]);
-  this.showObjective(rootBoard[0]);
+    var rootBoard = this.get('elementFactory').createRootBoard(name || '<TBD>');
+    this._definitions.get('rootBoards').push(rootBoard[0]);
+    this._definitions.get('rootElements').push(rootBoard[1]);
+    this.showObjective(rootBoard[0]);
 }
 OmModeler.prototype.deleteObjective = function (objective) {
 
-  var currentIndex = findIndex(this._definitions.get('rootElements'), objective.plane.boardElement);
-  this._definitions.get('rootElements').splice(currentIndex,1);
+    var currentIndex = findIndex(this._definitions.get('rootElements'), objective.plane.boardElement);
+    this._definitions.get('rootElements').splice(currentIndex, 1);
 
-  currentIndex = findIndex(this._definitions.get('rootBoards'), objective);
-  var indexAfterRemoval = Math.min(currentIndex, this._definitions.get('rootBoards').length - 2);
-  this._definitions.get('rootBoards').splice(currentIndex,1);
+    currentIndex = findIndex(this._definitions.get('rootBoards'), objective);
+    var indexAfterRemoval = Math.min(currentIndex, this._definitions.get('rootBoards').length - 2);
+    this._definitions.get('rootBoards').splice(currentIndex, 1);
 
-  if (this.getCurrentObjective() === objective) {
-    this.showObjective(this._definitions.get('rootBoards')[indexAfterRemoval]);
-  }
+    if (this.getCurrentObjective() === objective) {
+        this.showObjective(this._definitions.get('rootBoards')[indexAfterRemoval]);
+    }
 }
 
-OmModeler.prototype.handleOlcListChanged = function (olcs, dryRun=false) {
-  this._olcs = olcs;
+OmModeler.prototype.handleOlcListChanged = function (olcs, dryRun = false) {
+    this._olcs = olcs;
 }
 
 OmModeler.prototype.handleStateRenamed = function (olcState) {
-  this.getObjectsInState(olcState).forEach((element, gfx) =>
-      this.get('eventBus').fire('element.changed', {
-        element
-      })
-  );
+    this.getObjectsInState(olcState).forEach((element, gfx) =>
+        this.get('eventBus').fire('element.changed', {
+            element
+        })
+    );
 }
 
 OmModeler.prototype.handleStateDeleted = function (olcState) {
-  this.getObjectsInState(olcState).forEach((element, gfx) => {
-    element.businessObject.state = undefined;
-    this.get('eventBus').fire('element.changed', {
-      element
+    this.getObjectsInState(olcState).forEach((element, gfx) => {
+        element.businessObject.state = undefined;
+        this.get('eventBus').fire('element.changed', {
+            element
+        });
     });
-  });
 }
 
 OmModeler.prototype.handleClassRenamed = function (clazz) {
-  this.getObjectsOfClass(clazz).forEach((element, gfx) =>
-      this.get('eventBus').fire('element.changed', {
-        element
-      })
-  );
+    this.getObjectsOfClass(clazz).forEach((element, gfx) =>
+        this.get('eventBus').fire('element.changed', {
+            element
+        })
+    );
 }
 
 OmModeler.prototype.handleClassDeleted = function (clazz) {
-  this.getObjectsOfClass(clazz).forEach((element, gfx) =>
-      this.get('modeling').removeElements([element])
-  );
+    this.getObjectsOfClass(clazz).forEach((element, gfx) =>
+        this.get('modeling').removeElements([element])
+    );
 }
 
 OmModeler.prototype.getObjectsInState = function (olcState) {
-  return this.get('elementRegistry').filter((element, gfx) =>
-      is(element, 'om:Object') &&
-      element.businessObject.state === olcState
-  );
+    return this.get('elementRegistry').filter((element, gfx) =>
+        is(element, 'om:Object') &&
+        element.businessObject.state === olcState
+    );
 }
 
 OmModeler.prototype.getObjectsOfClass = function (clazz) {
-  return this.get('elementRegistry').filter((element, gfx) =>
-      is(element, 'om:Object') &&
-      clazz.id &&
-      element.businessObject.classRef?.id === clazz.id
-  );
+    return this.get('elementRegistry').filter((element, gfx) =>
+        is(element, 'om:Object') &&
+        clazz.id &&
+        element.businessObject.classRef?.id === clazz.id
+    );
 }
 
 OmModeler.prototype.getObjectInstancesOfClass = function (clazz) {
-  let instances = this._definitions.get('objectInstances');
-  let result =  instances.filter((instance, gfx) =>
-      is(instance, 'om:ObjectInstance') &&
-      clazz.id &&
-      instance.classRef?.id === clazz.id
-  );
-  return result;
+    let instances = this._definitions.get('objectInstances');
+    let result = instances.filter((instance, gfx) =>
+        is(instance, 'om:ObjectInstance') &&
+        clazz.id &&
+        instance.classRef?.id === clazz.id
+    );
+    return result;
 }
