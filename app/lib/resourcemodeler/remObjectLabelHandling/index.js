4d3ef04a6362206c39361667874988835710f33e
Franka-T <frankatraupe@hotmail.com>
2023-05-02 14:00:44 +0200

    #131 next steps

--- app/lib/resourcemodeler/remObjectLabelHandling/index.js
+++
@@ -1,6 +0,0 @@
-import RemObjectLabelHandler from "./RemObjectLabelHandler";
-
-export default {
-    __init__: [ 'omObjectLabelHandler' ],
-    omObjectLabelHandler: [ 'type', RemObjectLabelHandler ]
-};
