4e37999794c50117f8913be42653bcd961ce34a9
Simon Nowack <simnow@gmx.de>
2023-05-04 15:17:19 +0200

    modified rem.json

---
+++ app/lib/resourcemodeler/remObjectLabelHandling/index.js
@@ -0,0 +1,6 @@
+import RemObjectLabelHandler from "./RemObjectLabelHandler";
+
+export default {
+    __init__: [ 'remObjectLabelHandler' ],
+    remObjectLabelHandler: [ 'type', RemObjectLabelHandler ]
+};
