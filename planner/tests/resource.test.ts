3530960dc3050b39c089382faccd74a5e0bda4b5
mariejarisch <marie.jarisch@student.hpi.uni-potsdam.de>
2023-05-23 13:24:41 +0200

    renaming of tests of type resource

--- planner/tests/resource.test.ts
+++ planner/tests/resource.test.ts
@@ -3,18 +3,23 @@ import {Role} from "../types/Role";
 
 let painter = new Role("painter");
 let picasso = new Resource("Picasso", [painter], 1);
+let tiler = new Role("tiler");
 
-describe('satisfies', () => {
+describe('satisfies function', () => {
 
-    test('resource should satisfy', () => {
+    test('resource that has the required role and a big enough capacity should satisfy the conditions', () => {
         expect(picasso.satisfies(painter, 1)).toEqual(true);
     });
 
-    test('resource should not satisfy', () => {
+    test('resource that has the required role but a too low capacity should not satisfy the conditions', () => {
         expect(picasso.satisfies(painter, 2)).toEqual(false);
     });
 
-    test('null should satisfy', () => {
+    test('resource that has a big enough capacity but not the required role should not satisfy the conditions', () => {
+        expect(picasso.satisfies(tiler, 1)).toEqual(false);
+    });
+
+    test('role null should satisfy the conditions', () => {
         expect(picasso.satisfies(null, 1)).toEqual(true); // NoP muss später wahrscheinlich noch angepasst werden
     });
 });
